c2723c2b82ba65c9b9a1089f1fc4b5de122b554d
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2025-10-28 17:13:47 -0300

    Remove Twitter widget script from viglet.js
    Eliminated the Twitter widget initialization code from viglet.js, simplifying the script and removing external dependency on Twitter widgets.

--- static_files/js/viglet.js
+++ static_files/js/viglet.js
@@ -15,21 +15,8 @@ $(function () {
         }
     });
 });
-window.twttr = (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0]
-        , t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    t._e = [];
-    t.ready = function (f) {
-        t._e.push(f);
-    };
-    return t;
-}(document, "script", "twitter-wjs"));
-(function (i, s, o, g, r, a, m) {
+
+$(function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
     i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
